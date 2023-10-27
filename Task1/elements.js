7a9997300b35d04ba825c614f2863f930db25aef
Ghaith <noctis_7@Ghaiths-MacBook-Pro.local>
2023-10-27 19:42:30 +0300

    JS4 EX1

--- Task1/elements.js
+++ Task1/elements.js
@@ -1,33 +1,25 @@
+
+
+
+
+function details(header, description) {
+        this.header = header;
+        this.description = description;
+}
+
+
+
+
+
 let exercises = [
-    {
-        "header" : "Chest day",
-        "description" : "Bench press, Inclined bench press, Declined bench press, Pec flies, Tricep extensions, Dips"
-
-    },
-
-    {
-        "header" : "Back day",
-        "description" : "Pull ups, Wide pull ups, Chin ups, Low rows, Med rows, Hyperextension, Bicep curls, Hammer curls"
-
-    },
-
-    {
-        "header" : "Shoulders day",
-        "description" : "Lateral raises, Shoulder press, Front raises, Rear delt, Traps"
-
-    },
-    {
-        "header" : " Legs day",
-        "description" : "Leg extensions, Leg press, Hamstring, Calves, Squat, Set ups"
-
-    }
+        new details("Chest day","Bench press, Inclined bench press, Declined bench press, Pec flies, Tricep extensions, Dips"),
+        new details("Back day","Pull ups, Wide pull ups, Chin ups, Low rows, Med rows, Hyperextension, Bicep curls, Hammer curls"),
+        new details("Shoulders day","Lateral raises, Shoulder press, Front raises, Rear delt, Traps"),
+        new details("Legs day","Leg extensions, Leg press, Hamstring, Calves, Squat, Set ups")
 ]
 
-
-
-function fillServicesInThePage(){
+function write(){
     document.getElementById("Days")
-
     for(let exercise of exercises){
         let content = `
                 <div>
@@ -39,11 +31,4 @@ function fillServicesInThePage(){
 
     }
 }
-
-fillServicesInThePage()
-
-function fillProjectsInThePage(){
-    document.getElementById("container").innerHTML = ""
-
- 
-}
+write()
